ae07bd32e605ef1bb78e8870174cb7a23f0f62e7
SnowflakeKailash <129992822+SnowflakeKailash@users.noreply.github.com>
2023-04-06 00:44:26 +0530

    Update streamlit_app.py
    Imported excel file

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+impport pandas
 
 streamlit.title("test the changes again");
 
@@ -7,3 +8,5 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")

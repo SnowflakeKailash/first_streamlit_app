2ecc4e7e668c14f7789a721c19d816bdcee51a55
SnowflakeKailash <129992822+SnowflakeKailash@users.noreply.github.com>
2023-04-06 21:39:44 +0530

    Printing the status

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit
 import pandas
+import requests
+
 
 streamlit.title("test the changes again");
 
@@ -20,3 +22,7 @@ fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.in
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
+
+#Printing the status
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)

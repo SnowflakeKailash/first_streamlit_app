5a963d8fb0c82323684301e9f8b3714314bd6ac5
SnowflakeKailash <129992822+SnowflakeKailash@users.noreply.github.com>
2023-04-07 12:20:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,6 +32,9 @@ else:
 #Printing the status
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 
+expect URLError as e:
+     streamlit.error()
+
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
